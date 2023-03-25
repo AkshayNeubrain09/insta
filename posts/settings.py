222e4aad8f39450c6c3a31807e71f47568226616
AkshayNeubrain09 <123366698+AkshayNeubrain09@users.noreply.github.com>
2023-03-25 12:16:40 +0530

    Update settings.py

--- posts/settings.py
+++ posts/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-^4xn6sx!=$aa%3gksdn(!*4f_3s+&lm*2j8gc9jc28-3x+@mkj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.111.197.160']
 
 
 # Application definition
